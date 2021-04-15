6ed6d50572ddf7c6e75dbfbe9742441db00ab750
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-04-14 20:05:37 -0400

    js homework

---
+++ random.js
@@ -0,0 +1,37 @@
+var a = 1;
+var b = 11;
+var c = 12;
+var d = 13; 
+var e = 14;
+var f = 15;
+var g = 16;
+var h = 17; 
+var i = 18;
+var j = 19; 
+
+//two times addition
+console.log(a+j);
+console.log(c+d);
+
+
+//two times subtraction
+console.log(b-a);
+console.log(c-b);
+
+
+//two times multiplication
+console.log(c*c)
+console.log(b*b)
+
+//two times division
+console.log(e/a)
+console.log(j/a)
+
+
+//two times increment 
+console.log(++a)
+console.log(++b)
+
+//two times decrement
+console.log(--f)
+console.log(--d)
